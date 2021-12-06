f6822091837b31fb997093ddb6405f30826ccd38
ethanrugh <erock1114@yahoo.com>
2021-12-05 23:33:27 -0600

    gget items from local, set items on page, dynamic time slot colors

---
+++ README.md
@@ -0,0 +1,20 @@
+# Work Day Scheduler
+
+## An application which allows you to plan out your work day with a handy day planner! Enter your to-do list for the appropriate hour, hit the save button, and your list won't dissapear upon refresh. Time slots are dynamically color-coded: green for in the future, red for in the present, and white for in the past. With a lot of help from my classmates, I have written code that I'm sure will be an excellent reference in the future. This challenge has helped me bridge the gap in my understanding of local storage, and javascript as a whole seems a lot less daunting now. Thanks for your consideration, and happy coding.
+
+
+##
+Buit with:
+    - CSS
+    - HTML
+    - JS
+    - Moment
+    - VS Code
+
+##
+link to Work Day Scheduler webpage:
+https://ethanrugh.github.io/work-day-scheduler/
+
+##
+Screenshot:
+<img width="1440" alt="Screen Shot 2021-12-05 at 5 29 02 PM" src="https://user-images.githubusercontent.com/90535512/144768394-566cc93c-a67a-4c9a-bd28-4812aadd0c9e.png">
--- assets/js/script.js
+++ assets/js/script.js
@@ -3,16 +3,18 @@ let today = document.getElementById("currentDay");
 let date = moment().format("MMM Do YY"); 
 // add date to html page
 today.innerHTML = date;
+let hour = moment().hour()
+console.log(hour);
 
-let box9El = document.getElementById("box9");
-let box10El = document.getElementById("box10");
-let box11El = document.getElementById("box11");
-let box12El = document.getElementById("box12");
-let box1El = document.getElementById("box1");
-let box2El = document.getElementById("box2");
-let box3El = document.getElementById("box3");
-let box4El = document.getElementById("box4");
-let box5El = document.getElementById("box5");
+let box9El = document.getElementById("9");
+let box10El = document.getElementById("10");
+let box11El = document.getElementById("11");
+let box12El = document.getElementById("12");
+let box1El = document.getElementById("1");
+let box2El = document.getElementById("2");
+let box3El = document.getElementById("3");
+let box4El = document.getElementById("4");
+let box5El = document.getElementById("5");
 
 let saveBtn9EL = document.getElementById("save-btn9");
 let saveBtn10EL = document.getElementById("save-btn10");
@@ -24,74 +26,93 @@ let saveBtn3EL = document.getElementById("save-btn3");
 let saveBtn4EL = document.getElementById("save-btn4");
 let saveBtn5EL = document.getElementById("save-btn5");
 
-//make an array
-let listEl = [];
-
 let save9 = function() {
     let listItem9 = box9El.value;
-    listEl.push(listItem9);
-    console.log(listEl);
-    localStorage.setItem("listItem9", listEl);
-
+    localStorage.setItem("listItem9", JSON.stringify(listItem9));
 };
 
 let save10 = function() {
     let listItem10 = box10El.value;
-    listEl.push(listItem10);
-    console.log(listEl);
-    localStorage.setItem("listItem10", listEl);
+    localStorage.setItem("listItem10", JSON.stringify(listItem10));
 };
 
 let save11 = function() {
     let listItem11 = box11El.value;
-    listEl.push(listItem11);
-    console.log(listEl);
-    localStorage.setItem("listItem11", listEl);
+    localStorage.setItem("listItem11", JSON.stringify(listItem11));
 };
 
 let save12 = function() {
     let listItem12 = box12El.value;
-    listEl.push(listItem12);
-    console.log(listEl);
-    localStorage.setItem("listItem12", listEl);
+    localStorage.setItem("listItem12", JSON.stringify(listItem12));
 };
 
 let save1 = function() {
     let listItem1 = box1El.value;
-    listEl.push(listItem1);
-    console.log(listEl);
-    localStorage.setItem("listItem1", listEl);
+    localStorage.setItem("listItem1", JSON.stringify(listItem1));
 };
 
 let save2 = function() {
     let listItem2 = box2El.value;
-    listEl.push(listItem2);
-    console.log(listEl);
-    localStorage.setItem("listItem2", listEl);
+    localStorage.setItem("listItem2", JSON.stringify(listItem2));
 };
 
 let save3 = function() {
     let listItem3 = box3El.value;
-    listEl.push(listItem3);
-    console.log(listEl);
-    localStorage.setItem("listItem3", listEl);
+    localStorage.setItem("listItem3", JSON.stringify(listItem3));
 };
 
 let save4 = function() {
     let listItem4 = box4El.value;
-    listEl.push(listItem4);
-    console.log(listEl);
-    localStorage.setItem("listItem4", listEl);
+    localStorage.setItem("listItem4", JSON.stringify(listItem4));
 };
 
 let save5 = function() {
     let listItem5 = box5El.value;
-    listEl.push(listItem5);
-    console.log(listEl);
-    localStorage.setItem("listItem5", listEl);
+    localStorage.setItem("listItem5", JSON.stringify(listItem5));
 };
 
+let displayListEl = function() {
+    var storageItem9 = JSON.parse(localStorage.getItem("listItem9"))
+    box9El.innerHTML = storageItem9;
+    var storageItem10 = JSON.parse(localStorage.getItem("listItem10"))
+    box10El.innerHTML = storageItem10;
+    var storageItem11 = JSON.parse(localStorage.getItem("listItem11"))
+    box11El.innerHTML = storageItem11;
+    var storageItem12 = JSON.parse(localStorage.getItem("listItem12"))
+    box12El.innerHTML = storageItem12;
+    var storageItem1 = JSON.parse(localStorage.getItem("listItem1"))
+    box1El.innerHTML = storageItem1;
+    var storageItem2 = JSON.parse(localStorage.getItem("listItem2"))
+    box2El.innerHTML = storageItem2;
+    var storageItem3 = JSON.parse(localStorage.getItem("listItem3"))
+    box3El.innerHTML = storageItem3;
+    var storageItem4 = JSON.parse(localStorage.getItem("listItem4"))
+    box4El.innerHTML = storageItem4;
+    var storageItem5 = JSON.parse(localStorage.getItem("listItem5"))
+    box5El.innerHTML = storageItem5;
+};
 
+let colorChanger = function() {
+    for (i = 0; i < 9; i++) {
+        var checkTime = i+9;
+        var textAreaId = ("#") + checkTime;
+        console.log(checkTime, textAreaId, hour);
+        
+        if (checkTime < hour) {
+            $(textAreaId).addClass("future");
+            console.log("first if statement");
+        }
+        if (checkTime > hour) {
+            $(textAreaId).addClass("past");
+            console.log("second if statement")
+        }
+        else {
+            $(textAreaId).addClass("present");
+        }
+    }
+}
+
+colorChanger();
 saveBtn9EL.addEventListener("click", save9);
 saveBtn10EL.addEventListener("click", save10);
 saveBtn11EL.addEventListener("click", save11);
@@ -102,29 +123,4 @@ saveBtn3EL.addEventListener("click", save3);
 saveBtn4EL.addEventListener("click", save4);
 saveBtn5EL.addEventListener("click", save5);
 
-
-
-// $(document).ready(function() {
-//     $("textarea").blur(function() {
-//         let listItem = toDo.value;
-//         if ()
-//         listEl.push(listItem);
-//         console.log(listEl);
-//     });
-// });
-
-// $(document).on("click", ".saveBtn", function(event){
-//     //console.log(toDo.value);
-//     for (i = 0; i < 9; i++) {
-//         if(toDo.value == "") {
-//             alert("please enter something");
-//         }
-//         else{
-//             localStorage.setItem("textbox", JSON.stringify(listEl));
-//             console.log(listEL);
-//         }
-//     }
-//     //alert(localStorage.getItem("textarea"))
-// });
-
-// add objects --need value of textarea elements..
+displayListEl();
